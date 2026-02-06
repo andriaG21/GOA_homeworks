42e33841d176d0e70b80048032cd288870aaaf25
andriaG21 <andriaguramishvili376@gmail.com>
2026-02-06 17:03:38 +0400

    commit

---
+++ level 1/classwork/cw.py
@@ -0,0 +1,6 @@
+name = 'Andria'
+lastName = 'guramishvili'
+age = 13
+print(name)
+print(lastName)
+print(age)
---
+++ level 1/homework/hw.py
@@ -0,0 +1,41 @@
+# data types - მონაცემთა ტიპები
+# string - ეწოდება ბრჭყალებში მოქცეულ მონაცემს
+# integer - ეწოდება მთელ რიცხვებს (9,55,67,28), integer ტიპს არ სჭირდება ბრჭყალები
+# float - ეწოდებათ ათწილად რიცხვებს (35,5 ,12,7 ,0,5), float ტიპს არ სჭირდება ბრჭყალები
+
+
+# string
+
+print('i love goa')
+print('football is best')
+print('i work hard')
+print('i like programing')
+print('i learn at goa')
+
+# integer
+
+print(15)
+print(99)
+print(21)
+print(25)
+print(63)
+
+# float
+
+print(37.4)
+print(54.6)
+print(87.2)
+print(0.9)
+print(9.3)
+
+
+name = 'andria'
+lastname = 'guramishvili'
+age = 13
+country = 'georgia'
+city = 'tbilisi'
+print(name)
+print(lastname)
+print(age)
+print(country)
+print(city)
