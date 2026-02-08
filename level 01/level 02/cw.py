27e2d8e3f2633b04934267332d0b37ee8323ac26
andriaG21 <andriaguramishvili376@gmail.com>
2026-02-08 20:43:46 +0400

    commit

--- level 01/level 02/cw.py
+++ level 01/level 02/cw.py
@@ -6,3 +6,16 @@ print(num1+num2)
 print(num1-num2)
 print(num1*num2)
 print(num1//num2)
+sum=8+9
+subtract=55-32
+multiplied=12*3
+divided=66//11
+print(sum)
+print(subtract)
+print(multiplied)
+print(divided)
+age=13
+age=age+35
+age=age-20
+age=age*20
+print(age)
