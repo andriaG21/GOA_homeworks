75466e81a0454fed5348173dfb77a646fcddbed5
andriaG21 <andriaguramishvili376@gmail.com>
2026-02-18 17:23:50 +0400

    commit

---
+++ level 04/classwok/main.py
@@ -0,0 +1,8 @@
+name = 'andria'
+surname = 'guramishvili'
+country = 'georgia'
+
+print('hello my name is' + ' ' + name + ' ' + surname + ' ' + "and i'm from" + ' ' + country)
+
+# coding-ის სამი ნაბიჯია - writing, sending და debugging
+# debugging - არის შეცდომის გასწორება
---
+++ level 04/homework/main.py
@@ -0,0 +1,28 @@
+# კონკატინაცია არის სტრინგების დაკავშირება
+
+name = 'andria'
+surname = 'guramishvili'
+country = 'georgia'
+fav_subject = 'sport'
+print('hello my name is' + ' ' + name + ' ' + surname + ' ' + 'i am from' + ' ' + country + ' ' + 'and my favourite subject is' + ' ' + fav_subject)
+
+my_group = 'group 94'
+text = 'my group is'
+print(my_group + ' ' + text)
+
+# coding three step - writing (წერა), sending (გაგზავნა), debugging
+
+# debugging - არის შეცდომის, error-ის გამოსწორება 
+
+academy1 = 'GOA'
+group = 94
+print(academy1)
+print(group)
+
+print(16+47)
+print(63-29)
+print(7*6)
+print(102/7)
+print(33//11)
+print(31%6)
+print(3**4)
