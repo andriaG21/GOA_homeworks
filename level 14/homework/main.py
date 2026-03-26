903ec261081d5f0fbd1c257543e50e2c8249296f
andriaG21 <andriaguramishvili376@gmail.com>
2026-03-26 13:23:56 +0400

    commit

---
+++ level 14/homework/main.py
@@ -0,0 +1,19 @@
+phrase = 'python programing'
+print(phrase[:6])
+print(phrase[7:])
+print(phrase[::3])
+
+text = 'phythonslicing'
+print(text[0:6])
+print(text[::-1])
+print(text[::2])
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(letters[::2])
+print(letters[2:7:2])
+print(letters[-5:-10:-1])
+
+sentence = 'slicing makes phython powerful'
+print(sentence[14:21])
+print(sentence[:-9:-1])
+print(sentence[::3])
