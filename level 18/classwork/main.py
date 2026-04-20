7364a890945d6142679a8568c4cc517864ae16b3
andriaG21 <andriaguramishvili376@gmail.com>
2026-04-20 21:01:59 +0400

    commit

---
+++ level 18/classwork/main.py
@@ -0,0 +1,24 @@
+# 1
+
+def greet(name):
+    print (f"hello {name}")
+
+greet('andria')
+
+# 2
+
+def numbers(x, y):
+    print (x + y)
+
+numbers(5, 10)
+
+# 3
+
+def even_or_odd(num):
+    if num % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+    
+even_or_odd
+    
